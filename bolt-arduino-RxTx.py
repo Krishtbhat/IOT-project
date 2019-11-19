af95bd72968eca8673db68c7c8e9c8b59c7f43fc
krishnatcs17 <krishnat.cs17@bmsce.ac.in>
2019-11-19 15:12:22 +0530

    .

--- bolt-arduino-RxTx.py
+++ bolt-arduino-RxTx.py
@@ -1,25 +1,30 @@
-from boltiot import Bolt
+from boltiot import boltiot
+import telegram
 import json, time
 from telgram_credentials.conf_telegram import *
-import subprocess, requests, logging
+import subprocess, requests
 
 api_key = "9555ccc7-de9e-4493-bf4d-bca28cd5d96d"
 device_id = "BOLT3848004"
 mb = Bolt(api_key, device_id) 
 
-def send_telegram_message(message):
+bot = telegram.Bot(token='1031896771:AAFlMFR1Q7Z8kdxD9M4MNiPaUZkMBl7hv4I')
+
+def send_telegram_message(message, imageNum):
     """Sends message via Telegram"""
     url = "https://api.telegram.org/" + telegram_bot_id + "/sendMessage"
     data = {
         "chat_id": telegram_chat_id,
         "text": message
     }
+    
     try:
         response = requests.request(
             "GET",
             url,
             params=data
         )
+        bot.send_photo(chat_id="@alerts_boltIOT19", photo=open('./images/'+ imageNum +'.bmp', 'rb'))
         print("This is the Telegram response")
         print(response.text)
         telegram_data = json.loads(response.text)
@@ -31,10 +36,12 @@ def send_telegram_message(message):
 
 
 response = mb.serialBegin('9600')
+with open("fnum.txt", 'r') as f:
+    imageNum = len(f.read().split())
 while True:
-    response1 = mb.serialRead('10') 
+    # response1 = mb.serialRead('10') 
     dread = mb.digitalRead("1")
-    data = json.loads(response1)
+    # data = json.loads(response1)
     dread1 = json.loads(dread)
     print(dread)
     
@@ -44,8 +51,7 @@ while True:
         command = '''C:
 cd/
 cd \"Program Files (x86)\"\\Java\\jdk1.8.0_221\\bin
-java code.SimpleRead
-'''
+java code.SimpleRead ''' + imageNum
         pro = subprocess.Popen('cmd.exe', stdin=subprocess.PIPE, shell=True)
         _, _ = pro.communicate(command.encode("utf-8"))
         print("Done!!")
@@ -56,8 +62,8 @@ java code.SimpleRead
 Click on the following link to display a message.
 https://cloud.boltiot.com/control?name=BOLT3848004"""
 
-        # telegram_status = send_telegram_message(message)
-        # print("This is the Telegram status:", telegram_status)
+        telegram_status = send_telegram_message(message, imageNum)
+        print("This is the Telegram status:", telegram_status)
    
         start = time.perf_counter()
         while(start + 10.0 > time.perf_counter() and json.loads(mb.digitalRead('1'))["value"] != '1'):
