5b83d172f13f3e3cd4fdb5f6d38db81b9d58c235
Krishna T <45008559+Krishtbhat@users.noreply.github.com>
2020-06-22 23:06:10 +0530

    Update conf_telegram.py

--- telgram_credentials/conf_telegram.py
+++ telgram_credentials/conf_telegram.py
@@ -1,6 +1,6 @@
-bolt_api_key = "9555ccc7-de9e-4493-bf4d-bca28cd5d96d"
-device_id = "BOLT3848004"
+bolt_api_key = "XXXXXXXX-XXXX-XXXX-XXXX-bca28cd5d96d"
+device_id = "BOLTXXXXXXX"
 telegram_chat_id = "@alerts_boltIOT19"
-telegram_bot_id = "bot1031896771:AAFlMFR1Q7Z8kdxD9M4MNiPaUZkMBl7hv4I"
+telegram_bot_id = "bot1031896771:AAFlMFR1Q7XXXXXXXXXXXXXXXXXXXXXXXXX"
 
 threshold = 100 
